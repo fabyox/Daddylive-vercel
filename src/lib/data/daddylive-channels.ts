769f825920a4decaf52cc54e4ca926c8aa65e7b8
fabyox <fabiogiordano1000@gmail.com>
2026-03-31 20:31:42 +0200

    Update daddylive-channels.ts

--- src/lib/data/daddylive-channels.ts
+++ src/lib/data/daddylive-channels.ts
@@ -2,351 +2,351 @@
 import { Channel } from '../types/channel';
 
 export const DADDYLIVE_CHANNELS: Channel[] = [
-  // Sports Channels from dlhd.so
+  // Sports Channels from dlstreams.top
   {
     name: "ESPN",
-    url: "https://dlhd.so/embed/stream-1.php",
+    url: "https://dlstreams.top/embed/stream-1.php",
     group: "Sports",
     logo: "https://logos-world.net/wp-content/uploads/2021/08/ESPN-Logo.png",
     tvgId: "espn.us"
   },
   {
     name: "ESPN 2",
-    url: "https://dlhd.so/embed/stream-2.php",
+    url: "https://dlstreams.top/embed/stream-2.php",
     group: "Sports", 
     logo: "https://logos-world.net/wp-content/uploads/2021/08/ESPN2-Logo.png",
     tvgId: "espn2.us"
   },
   {
     name: "Fox Sports 1",
-    url: "https://dlhd.so/embed/stream-3.php",
+    url: "https://dlstreams.top/embed/stream-3.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/commons/7/7d/Fox_Sports_1_logo.svg",
     tvgId: "fs1.us"
   },
   {
     name: "Fox Sports 2",
-    url: "https://dlhd.so/embed/stream-4.php",
+    url: "https://dlstreams.top/embed/stream-4.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/commons/8/8c/Fox_Sports_2_logo.svg",
     tvgId: "fs2.us"
   },
   {
     name: "NBC Sports",
-    url: "https://dlhd.so/embed/stream-5.php",
+    url: "https://dlstreams.top/embed/stream-5.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/commons/9/9c/NBC_Sports_logo.svg",
     tvgId: "nbcsports.us"
   },
   {
     name: "TNT Sports",
-    url: "https://dlhd.so/embed/stream-6.php",
+    url: "https://dlstreams.top/embed/stream-6.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/commons/2/24/TNT_Logo_2016.svg",
     tvgId: "tnt.us"
   },
   {
     name: "TBS",
-    url: "https://dlhd.so/embed/stream-7.php",
+    url: "https://dlstreams.top/embed/stream-7.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/commons/d/dc/TBS_logo_2016.svg",
     tvgId: "tbs.us"
   },
   {
     name: "Sky Sports Premier League",
-    url: "https://dlhd.so/embed/stream-8.php",
+    url: "https://dlstreams.top/embed/stream-8.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/en/1/1e/Sky_Sports_Premier_League.png",
     tvgId: "skypl.uk"
   },
   {
     name: "Sky Sports Football",
-    url: "https://dlhd.so/embed/stream-9.php",
+    url: "https://dlstreams.top/embed/stream-9.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/en/d/d8/Sky_Sports_Football.png",
     tvgId: "skyfoot.uk"
   },
   {
     name: "Sky Sports Main Event",
-    url: "https://dlhd.so/embed/stream-10.php",
+    url: "https://dlstreams.top/embed/stream-10.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/en/f/f4/Sky_Sports_Main_Event.png",
     tvgId: "skyme.uk"
   },
   {
     name: "BT Sport 1",
-    url: "https://dlhd.so/embed/stream-11.php",
+    url: "https://dlstreams.top/embed/stream-11.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/en/5/54/BT_Sport_logo_2019.svg",
     tvgId: "bts1.uk"
   },
   {
     name: "BT Sport 2",
-    url: "https://dlhd.so/embed/stream-12.php",
+    url: "https://dlstreams.top/embed/stream-12.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/en/5/54/BT_Sport_logo_2019.svg",
     tvgId: "bts2.uk"
   },
   {
     name: "Eurosport 1",
-    url: "https://dlhd.so/embed/stream-13.php",
+    url: "https://dlstreams.top/embed/stream-13.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/commons/d/d8/Eurosport_1_logo.svg",
     tvgId: "eurosport1"
   },
   {
     name: "Eurosport 2",
-    url: "https://dlhd.so/embed/stream-14.php",
+    url: "https://dlstreams.top/embed/stream-14.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/commons/9/9e/Eurosport_2.svg",
     tvgId: "eurosport2"
   },
   {
     name: "beIN Sports 1",
-    url: "https://dlhd.so/embed/stream-15.php",
+    url: "https://dlstreams.top/embed/stream-15.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/commons/b/bb/BeIN_Sports_logo.svg",
     tvgId: "bein1"
   },
   {
     name: "beIN Sports 2",
-    url: "https://dlhd.so/embed/stream-16.php",
+    url: "https://dlstreams.top/embed/stream-16.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/commons/b/bb/BeIN_Sports_logo.svg",
     tvgId: "bein2"
   },
   {
     name: "NBA TV",
-    url: "https://dlhd.so/embed/stream-17.php",
+    url: "https://dlstreams.top/embed/stream-17.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/en/d/d2/NBA_TV.svg",
     tvgId: "nba.tv"
   },
   {
     name: "NFL Network",
-    url: "https://dlhd.so/embed/stream-18.php",
+    url: "https://dlstreams.top/embed/stream-18.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/en/0/0a/NFL_Network_logo.svg",
     tvgId: "nfl.network"
   },
   {
     name: "NHL Network",
-    url: "https://dlhd.so/embed/stream-19.php",
+    url: "https://dlstreams.top/embed/stream-19.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/en/f/f4/NHL_Network.svg",
     tvgId: "nhl.network"
   },
   {
     name: "MLB Network",
-    url: "https://dlhd.so/embed/stream-20.php",
+    url: "https://dlstreams.top/embed/stream-20.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/en/4/4a/MLB_Network_Logo.svg",
     tvgId: "mlb.network"
   },
   {
     name: "Golf Channel",
-    url: "https://dlhd.so/embed/stream-21.php",
+    url: "https://dlstreams.top/embed/stream-21.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/commons/0/07/Golf_Channel_logo.svg",
     tvgId: "golf.channel"
   },
   {
     name: "Tennis Channel",
-    url: "https://dlhd.so/embed/stream-22.php",
+    url: "https://dlstreams.top/embed/stream-22.php",
     group: "Sports",
     logo: "https://upload.wikimedia.org/wikipedia/en/1/10/Tennis_Channel_logo.svg",
     tvgId: "tennis.channel"
   },
 
-  // News Channels from dlhd.so
+  // News Channels from dlstreams.top
   {
     name: "CNN",
-    url: "https://dlhd.so/embed/stream-23.php",
+    url: "https://dlstreams.top/embed/stream-23.php",
     group: "News",
     logo: "https://upload.wikimedia.org/wikipedia/commons/b/b1/CNN.svg",
     tvgId: "cnn.us"
   },
   {
     name: "Fox News",
-    url: "https://dlhd.so/embed/stream-24.php",
+    url: "https://dlstreams.top/embed/stream-24.php",
     group: "News",
     logo: "https://upload.wikimedia.org/wikipedia/commons/6/67/Fox_News_Channel_logo.svg",
     tvgId: "foxnews.us"
   },
   {
     name: "MSNBC",
-    url: "https://dlhd.so/embed/stream-25.php",
+    url: "https://dlstreams.top/embed/stream-25.php",
     group: "News",
     logo: "https://upload.wikimedia.org/wikipedia/commons/1/11/MSNBC_logo.svg",
     tvgId: "msnbc.us"
   },
   {
     name: "BBC News",
-    url: "https://dlhd.so/embed/stream-26.php",
+    url: "https://dlstreams.top/embed/stream-26.php",
     group: "News",
     logo: "https://upload.wikimedia.org/wikipedia/commons/6/62/BBC_News_2019.svg",
     tvgId: "bbcnews.uk"
   },
   {
     name: "Sky News",
-    url: "https://dlhd.so/embed/stream-27.php",
+    url: "https://dlstreams.top/embed/stream-27.php",
     group: "News",
     logo: "https://upload.wikimedia.org/wikipedia/en/6/65/Sky-news-logo.svg",
     tvgId: "skynews.uk"
   },
   {
     name: "Al Jazeera English",
-    url: "https://dlhd.so/embed/stream-28.php",
+    url: "https://dlstreams.top/embed/stream-28.php",
     group: "News",
     logo: "https://upload.wikimedia.org/wikipedia/en/f/f2/Aljazeera_eng.png",
     tvgId: "aljazeera.en"
   },
 
-  // Entertainment Channels from dlhd.so
+  // Entertainment Channels from dlstreams.top
   {
     name: "HBO",
-    url: "https://dlhd.so/embed/stream-29.php",
+    url: "https://dlstreams.top/embed/stream-29.php",
     group: "Entertainment",
     logo: "https://upload.wikimedia.org/wikipedia/commons/d/de/HBO_logo.svg",
     tvgId: "hbo.us"
   },
   {
     name: "HBO Max",
-    url: "https://dlhd.so/embed/stream-30.php",
+    url: "https://dlstreams.top/embed/stream-30.php",
     group: "Entertainment",
     logo: "https://upload.wikimedia.org/wikipedia/commons/1/17/HBO_Max_Logo.svg",
     tvgId: "hbomax.us"
   },
   {
     name: "Showtime",
-    url: "https://dlhd.so/embed/stream-31.php",
+    url: "https://dlstreams.top/embed/stream-31.php",
     group: "Entertainment",
     logo: "https://upload.wikimedia.org/wikipedia/commons/2/22/Showtime.svg",
     tvgId: "showtime.us"
   },
   {
     name: "FX",
-    url: "https://dlhd.so/embed/stream-32.php",
+    url: "https://dlstreams.top/embed/stream-32.php",
     group: "Entertainment",
     logo: "https://upload.wikimedia.org/wikipedia/commons/4/4d/FX_International_logo.svg",
     tvgId: "fx.us"
   },
   {
     name: "AMC",
-    url: "https://dlhd.so/embed/stream-33.php",
+    url: "https://dlstreams.top/embed/stream-33.php",
     group: "Entertainment",
     logo: "https://upload.wikimedia.org/wikipedia/commons/3/34/AMC_logo_2019.svg",  
     tvgId: "amc.us"
   },
   {
     name: "USA Network",
-    url: "https://dlhd.so/embed/stream-34.php",
+    url: "https://dlstreams.top/embed/stream-34.php",
     group: "Entertainment",
     logo: "https://upload.wikimedia.org/wikipedia/commons/d/d7/USA_Network_logo_%282016%29.svg",
     tvgId: "usa.us"
   },
   {
     name: "Comedy Central",
-    url: "https://dlhd.so/embed/stream-35.php",
+    url: "https://dlstreams.top/embed/stream-35.php",
     group: "Entertainment",
     logo: "https://upload.wikimedia.org/wikipedia/commons/a/aa/Comedy_Central_2018.svg",
     tvgId: "comedycentral.us"
   },
 
-  // Movie Channels from dlhd.so
+  // Movie Channels from dlstreams.top
   {
     name: "TCM",
-    url: "https://dlhd.so/embed/stream-36.php",
+    url: "https://dlstreams.top/embed/stream-36.php",
     group: "Movies",
     logo: "https://upload.wikimedia.org/wikipedia/commons/8/83/Turner_Classic_Movies_%28TCM%2C_current%29.svg",
     tvgId: "tcm.us"
   },
   {
     name: "IFC",
-    url: "https://dlhd.so/embed/stream-37.php",
+    url: "https://dlstreams.top/embed/stream-37.php",
     group: "Movies",
     logo: "https://upload.wikimedia.org/wikipedia/commons/5/59/IFC_logo.svg",
     tvgId: "ifc.us"
   },
   {
     name: "Lifetime",
-    url: "https://dlhd.so/embed/stream-38.php",
+    url: "https://dlstreams.top/embed/stream-38.php",
     group: "Movies",
     logo: "https://upload.wikimedia.org/wikipedia/commons/9/9c/Lifetime_logo_2020.svg",
     tvgId: "lifetime.us"
   },
 
-  // Kids Channels from dlhd.so
+  // Kids Channels from dlstreams.top
   {
     name: "Disney Channel",
-    url: "https://dlhd.so/embed/stream-39.php",
+    url: "https://dlstreams.top/embed/stream-39.php",
     group: "Kids",
     logo: "https://upload.wikimedia.org/wikipedia/commons/d/d2/2019_Disney_Channel_logo.svg",
     tvgId: "disney.us"
   },
   {
     name: "Nickelodeon",
-    url: "https://dlhd.so/embed/stream-40.php",
+    url: "https://dlstreams.top/embed/stream-40.php",
     group: "Kids",
     logo: "https://upload.wikimedia.org/wikipedia/commons/7/7a/Nickelodeon_2009_logo.svg",
     tvgId: "nick.us"
   },
   {
     name: "Cartoon Network",
-    url: "https://dlhd.so/embed/stream-41.php",
+    url: "https://dlstreams.top/embed/stream-41.php",
     group: "Kids",
     logo: "https://upload.wikimedia.org/wikipedia/commons/8/80/Cartoon_Network_2010_logo.svg",
     tvgId: "cartoonnetwork.us"
   },
 
-  // Documentary Channels from dlhd.so
+  // Documentary Channels from dlstreams.top
   {
     name: "Discovery Channel",
-    url: "https://dlhd.so/embed/stream-42.php",
+    url: "https://dlstreams.top/embed/stream-42.php",
     group: "Documentary",
     logo: "https://upload.wikimedia.org/wikipedia/commons/2/27/Discovery_Channel_-_Logo_2019.svg",
     tvgId: "discovery.us"
   },
   {
     name: "History Channel",
-    url: "https://dlhd.so/embed/stream-43.php",
+    url: "https://dlstreams.top/embed/stream-43.php",
     group: "Documentary",
     logo: "https://upload.wikimedia.org/wikipedia/commons/0/01/History_%282021%29.svg",
     tvgId: "history.us"
   },
   {
     name: "National Geographic",
-    url: "https://dlhd.so/embed/stream-44.php",
+    url: "https://dlstreams.top/embed/stream-44.php",
     group: "Documentary",
     logo: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Natgeologo.svg",
     tvgId: "natgeo.us"  
   },
   {
     name: "Animal Planet",
-    url: "https://dlhd.so/embed/stream-45.php",
+    url: "https://dlstreams.top/embed/stream-45.php",
     group: "Documentary",
     logo: "https://upload.wikimedia.org/wikipedia/commons/2/20/2018_Animal_Planet_logo.svg",
     tvgId: "animalplanet.us"
   },
 
-  // Music Channels from dlhd.so
+  // Music Channels from dlstreams.top
   {
     name: "MTV",
-    url: "https://dlhd.so/embed/stream-46.php",
+    url: "https://dlstreams.top/embed/stream-46.php",
     group: "Music",
     logo: "https://upload.wikimedia.org/wikipedia/commons/0/0d/MTV-2021.svg",
     tvgId: "mtv.us"
   },
   {
     name: "VH1",
-    url: "https://dlhd.so/embed/stream-47.php",
+    url: "https://dlstreams.top/embed/stream-47.php",
     group: "Music",
     logo: "https://upload.wikimedia.org/wikipedia/commons/9/94/VH1_logonew.svg",
     tvgId: "vh1.us"
   },
   {
     name: "BET",
-    url: "https://dlhd.so/embed/stream-48.php",
+    url: "https://dlstreams.top/embed/stream-48.php",
     group: "Music",
     logo: "https://upload.wikimedia.org/wikipedia/commons/6/64/Black_Entertainment_Television_%28logo%29.svg",
     tvgId: "bet.us"
